23b00097c06fa3ab5842fa35ddea82520201f37b
BiporshiChakma <biporshichakma277@gmail.com>
2023-05-29 22:02:52 +0600

    commit

---
+++ Request-Validation/app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+
+class RegistrationController extends Controller
+{
+    public function register(RegistrationRequest $request)
+    {
+        // The request has passed the validation rules.
+        // Proceed with the registration logic.
+    }
+}
+
+
